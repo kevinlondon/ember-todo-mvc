96c2d64ca82067314ab77e057ef1cd09b9165c02
kevinlondon <kevinlondon@gmail.com>
2015-08-03 17:02:42 -0700

    Reached the bittersweet end of the tutorial.

--- app/components/todo-list.js
+++ app/components/todo-list.js
@@ -5,12 +5,35 @@ export default Ember.Component.extend({
         createTodo: function(newTitle) {
             this.set('newTitle', '');
             this.sendAction('createTodo', newTitle);
+        },
+        clearCompleted: function() {
+            var completed = this.get('model').filterBy('isCompleted', true);
+            completed.invoke('deleteRecord');
+            completed.invoke('save');
         }
     },
+    hasCompleted: Ember.computed('completed', function() {
+        return this.get('completed') > 0;
+    }),
+    completed: Ember.computed('model.@each.isCompleted', function() {
+        var model = this.get('model');
+        return model.filterBy('isCompleted', true).get('length');
+    }),
     remaining: Ember.computed('model.@each.isCompleted', function() {
         var model = this.get('model');
         return model.filterBy('isCompleted', false).get('length');
     }),
+    allAreDone: function(key, value) {
+        var model = this.get('model');
+        console.log(key + ": " + value);
+        if (value === undefined) {
+            return model.get('length') > 0 && model.isEvery('isComplete', true);
+        } else {
+            model.setEach('isCompleted', value);
+            model.invoke('save');
+            return value;
+        }
+    }.property('@each.isCompleted'),
     inflection: Ember.computed('remaining', function() {
         var remaining = this.get('remaining');
         return (remaining === 1) ? 'item' : 'items';
